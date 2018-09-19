fe20782ee935f8c8b0675e6124a3d71db4432349
MrXiaoChu <MrXiaoChu@users.noreply.github.com>
2018-09-19 20:21:45 +0800

    Create mBot_03.c

---
+++ mBot_03.c
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <MeMCore.h>
+
+#define all 0
+#define m1 9
+#define m2 10
+#define go_front 1
+#define go_back 2
+#define go_left 3
+#define go_right 4
+#define led_port 7
+#define two_leds 0
+#define follower_connect_port 4
+
+
+MBotDCMotor motor_all(all);
+MBotDCMotor motor_m1(m1);
+MBotDCMotor motor_m2(m2);
+
+MeRGBLed led(led_port, 2);
+
+MeLineFollower lfer(follower_connect_port);
+
+
+
+int returnValue;
+
+void setup() {
+
+  motor_m1.run(-255);
+  delay(500);
+  motor_m1.run(0);
+  motor_m2.run(-255);
+  delay(500);
+  motor_m2.run(0);
+  motor_m2.run(255);
+  delay(500);
+  motor_m2.run(0);
+  motor_m1.run(255);
+  delay(500);
+  motor_m1.run(0);
+
+  motor_all.move(go_left, 255);
+  delay(200);
+  motor_all.move(go_right, 255);
+  delay(200);
+  motor_all.move(go_front, 0);
+
+  motor_all.move(go_left, 255);
+  delay(200);
+  motor_all.move(go_right, 255);
+  delay(200);
+  motor_all.move(go_front, 0);
+
+  motor_all.move(go_left, 255);
+  delay(200);
+  motor_all.move(go_right, 255);
+  delay(200);
+  motor_all.move(go_front, 0);
+
+  motor_all.move(go_left, 255);
+  delay(200);
+  motor_all.move(go_right, 255);
+  delay(200);
+  motor_all.move(go_front, 0);
+
+  motor_all.move(go_left, 255);
+  delay(200);
+  motor_all.move(go_right, 255);
+  delay(200);
+  motor_all.move(go_front, 0);
+
+  led.setColor(two_leds, 255, 0, 0);
+  led.show();
+  delay(1000);
+  led.setColor(two_leds, 0, 0, 0);
+  led.show();
+  led.setColor(two_leds, 0, 255, 0);
+  led.show();
+  delay(1000);
+  led.setColor(two_leds, 0, 0, 0);
+  led.show();
+  led.setColor(two_leds, 0, 0, 255);
+  led.show();
+  delay(1000);
+  led.setColor(two_leds, 0, 0, 0);
+  led.show();
+
+  int counter = 0;
+  while (counter < 3) {
+    led.setColor(two_leds, 255, 255, 255);
+    led.show();
+    delay(200);
+    led.setColor(two_leds, 0, 0, 0);
+    led.show();
+    delay(200);
+    ++counter;
+  }
+
+
+}
+
+void loop() {
+  /*
+     #define go_front 1
+     #define go_back 2
+     #define go_left 3
+     #define go_right 4
+  */
+  returnValue = lfer.readSensors();
+
+
+  if (returnValue == 0) {
+    motor_all.move(go_front, 255);
+  }
+  if (returnValue == 1) {
+    motor_all.move(go_left, 255);
+    while (lfer.readSensors() == 1);
+  }
+  if (returnValue == 2) {
+    motor_all.move(go_right, 255);
+    while (lfer.readSensors() == 2);
+  }
+
+}
